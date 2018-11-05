19338a89ef7ca6f8f62fc7b565499c6afa72caba
Cloud User <ec2-user@ip-172-31-23-165.eu-central-1.compute.internal>
2018-11-05 16:52:35 +0100

    first working

--- cma.py
+++ cma.py
@@ -1,64 +1,39 @@
 #!/usr/bin/env python
 import sys
 import argparse
-# import ConfigParser
+import ConfigParser
+import ssl
 from cm_api.api_client import ApiResource
-# from cm_api.endpoints.services import ApiService
-
-# cm_host = "cm-host"
-# api = ApiResource(cm_host, username="admin", password="admin")
-
-# # Get a list of all clusters
-# cdh4 = None
-# for c in api.get_all_clusters():
-  # print c.name
-  # if c.version == "CDH4":
-    # cdh4 = c
 
 def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument("action", help="stop or start the cluster")
 	args = parser.parse_args()
 	action = args.action
-	
+		
 	if (args.action == "start"):
-		print " ".join([action,"cluster"])
+		print " ".join([action +"ing" ,"cluster"])
 		# cluster.start().wait()
 	elif (args.action == "stop"):
-		print " ".join([action,"cluster"])
+		print " ".join([action+"ping","cluster"])
 		# cluster.stop().wait()
 	else:
 		print ("Action can be either start or stop")
 		sys.exit(1)
+
+	cfg = ConfigParser.ConfigParser()
+	cfg.read("cdh.cfg")
 	
+	cmhost = cfg.get("CM", "host")
+	user = cfg.get("CM", "username")
+	passw = cfg.get("CM", "password")
+	cafile = cfg.get("CM", "cafile")
+	
+	context = ssl.create_default_context(cafile=cafile)
 
-
-	# # Prep for reading config props from external file
-	# CONFIG = ConfigParser.ConfigParser()
-	# CONFIG.read("clouderaconfig.ini")
-
-	# ### Set up environment-specific vars ###
-
-	# # This is the host that the Cloudera Manager server is running on
-	# CM_HOST = CONFIG.get("CM", "cm.host")
-
-	# # CM admin account info
-	# ADMIN_USER = CONFIG.get("CM", "admin.name")
-	# ADMIN_PASS = CONFIG.get("CM", "admin.password")
-
-	# #### Cluster Definition #####
-	# CLUSTER_NAME = CONFIG.get("CM", "cluster.name")
-	# CDH_VERSION = "CDH5"
-
-	# API = ApiResource(CM_HOST, version=5, username=ADMIN_USER, password=ADMIN_PASS)
-	# print "Connected to CM host on " + CM_HOST
-
-	# CLUSTER = API.get_cluster(CLUSTER_NAME)
-
-	# print "About to restart cluster."
-	# # CLUSTER.restart().wait()
-	# print "Done restarting cluster."
-
+	api = ApiResource(cmhost, username=user, password=passw, ssl_context = context, use_tls = True)	
+	for c in api.get_all_clusters():
+		print c.name
 
 if __name__ == "__main__":
    main()
