88348a46659aecaabc13e3ad286bd78133909412
nmarian85 <ec2-user@ip-172-31-23-165.eu-central-1.compute.internal>
2018-11-08 19:14:39 +0100

    mp working

--- cma.py
+++ cma.py
@@ -3,21 +3,38 @@ import sys
 import argparse
 import ConfigParser
 import ssl
+import time
+import multiprocessing
 from cm_api.api_client import ApiResource
+
+def get_svc_action(c, action):
+	for s in c.get_all_services():
+		n = s.name
+		st = s.serviceState
+		print(n + " " + st)
+		if "CDSW" in n:
+			continue
+		if action not in st.lower():
+			print(n + " not in " + action + " state yet")
+			return False
+	return True
+		
+
+def do_actions(c, action):
+	if (action == "start"):
+		print("Starting cluster")
+		c.start().wait()
+	else:
+		print ("Stopping cluster")
+		c.stop().wait()
 
 def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument("action", help="stop or start the cluster")
 	args = parser.parse_args()
 	action = args.action
-		
-	if (args.action == "start"):
-		print " ".join([action +"ing" ,"cluster"])
-		# cluster.start().wait()
-	elif (args.action == "stop"):
-		print " ".join([action+"ping","cluster"])
-		# cluster.stop().wait()
-	else:
+	
+	if ((action != "start") and (action != "stop")):
 		print ("Action can be either start or stop")
 		sys.exit(1)
 
@@ -32,8 +49,20 @@ def main():
 	context = ssl.create_default_context(cafile=cafile)
 
 	api = ApiResource(cmhost, username=user, password=passw, ssl_context = context, use_tls = True)	
-	for c in api.get_all_clusters():
-		print c.name
-
+	allc = api.get_all_clusters()
+	c = allc[0]
+	
+	p = multiprocessing.Process(target=do_actions, args=(c, action))
+	p.start()
+	
+	timeout = time.time() + 60*10
+	while True:
+		if ((get_svc_action(c, action) == True ) or (time.time() > timeout)):
+			p.join()
+			break
+		time.sleep(10)
+	p.join()
+	
 if __name__ == "__main__":
-   main()
+	main()
+	
